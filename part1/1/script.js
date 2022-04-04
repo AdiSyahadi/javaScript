6f211ed16dd94ef810864a5089380e12c99c7e5f
Syahadi <54655295+AdiSyahadi@users.noreply.github.com>
2022-04-04 10:33:42 +0700

    update file

--- part1/1/script.js
+++ part1/1/script.js
@@ -1,4 +1,4 @@
-let js = 'juned';
+/*let js = 'juned';
 if (js === 'juned') alert('JavaScript keren');
 
 40 + 5 + 3;
@@ -21,4 +21,39 @@ let myCurrentJob = 'Teacher';
 let job1 = 'programmer';
 let job2 = 'teacher';
 
-console.log(myFirstJob);
+console.log(myFirstJob);*/
+
+
+// Type data di dalam JavaScript
+/*
+true;
+console.log(true)
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log('type data nya adalah : ',typeof true);
+console.log('type data nya adalah : ',typeof javascriptIsFun);
+console.log('type data nya adalah : ',typeof 23);
+console.log('type data nya adalah : ',typeof 23.0);
+console.log('type data nya adalah : ',typeof 'jonas');
+let joni;
+console.log('type data nya adalah : ',typeof joni);
+console.log(typeof null)
+
+
+//membuat variabel tanpa menggunakan let juga bisa
+//merubah type data dari variabel juga bisa dilakukan
+javascriptIsFun = 'Juned';
+console.log(javascriptIsFun); */
+
+// Penggunaan let, const, dan var dalam penulisan variabel
+
+let age = 30; //variabel yang bisa di update nilainya
+age= 31; //maka isi dari variabel age akan berubah/update menjadi 31
+
+const tahunLahir = 1996; //menggunakan const maka variabel yang  tidak bisa di update nilainya
+tahunLahir = 1997; // variabel tahunLahir tidak akan bisa di ubah/update
+
+var kerja = 'prorgamer' //variabel yang bisa di update nilainya
+kerja = 'pengajar' // maka akan berubah menjadi pengajar
