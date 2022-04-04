bec83ae28c9d4cff7c59e8a2390b38722159c498
adisyahadi40@gmail.com <yadisyahadi40@gmail.com>
2022-04-04 10:03:35 +0700

    part1

---
+++ part1/1/script.js
@@ -0,0 +1,24 @@
+let js = 'juned';
+if (js === 'juned') alert('JavaScript keren');
+
+40 + 5 + 3;
+console.log(40 + 3 + 10);
+
+let firstname = "adiSyahadi";
+let first = "Juned"
+let firstNamePerson //camelCase
+let first_name_person //Snack_case
+
+console.log(firstname);
+
+let 3years = 3; // penulisan variabel tidak boleh di awali angka
+let jonas&matlida = 'JN' // simbol & tidak boleh di gunakan untuk nama variabel
+let function = 27; // tidak boleh menggunakan keyword yang ada dalam JavaScript
+ 
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
